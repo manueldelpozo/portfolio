5073cee0de7d4217fd08b55afd9e480e8557118d
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2019-02-24 10:58:41 +0100

    Updates

---
+++ precache-manifest.6c0d556b9adbf6ecbd2116792564a97d.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/portfolio/static/media/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "18ec61d24771d9a73a28",
+    "url": "/portfolio/static/css/main.a1136cc0.chunk.css"
+  },
+  {
+    "revision": "698fcd0806ae1ca323e8e7d132d53087",
+    "url": "/portfolio/static/media/qsdm.698fcd08.png"
+  },
+  {
+    "revision": "9c4058a4080b54e7cd9a",
+    "url": "/portfolio/static/js/1.9c4058a4.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "6794c78f7802103deffbae816725af72",
+    "url": "/portfolio/static/media/CV_Manuel_delPozo_FrontEndDeveloper.6794c78f.pdf"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/portfolio/static/media/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/portfolio/static/media/roboto-latin-100.987b8457.woff2"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/portfolio/static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/portfolio/static/media/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/portfolio/static/media/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/portfolio/static/media/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/portfolio/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/portfolio/static/media/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/portfolio/static/media/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/portfolio/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/portfolio/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/portfolio/static/media/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/portfolio/static/media/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/portfolio/static/media/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/portfolio/static/media/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/portfolio/static/media/roboto-latin-300.a1471d1d.woff"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/portfolio/static/media/roboto-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/portfolio/static/media/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "18ec61d24771d9a73a28",
+    "url": "/portfolio/static/js/main.18ec61d2.chunk.js"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/portfolio/static/media/roboto-latin-900.8c2ade50.woff"
+  },
+  {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/portfolio/static/media/roboto-latin-700italic.846d1890.woff"
+  },
+  {
+    "revision": "7b770d6c53423deb1a8e49d3c9175184",
+    "url": "/portfolio/static/media/roboto-latin-900italic.7b770d6c.woff2"
+  },
+  {
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/portfolio/static/media/roboto-latin-900italic.bc833e72.woff"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/portfolio/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "9f23b994ff1db4b7094a91ff78cb8fae",
+    "url": "/portfolio/static/media/CNAME.9f23b994.bin"
+  },
+  {
+    "revision": "f23d2782bd0a7cf0ad5c38e73f8c75ad",
+    "url": "/portfolio/static/media/profile.f23d2782.png"
+  },
+  {
+    "revision": "2b0c9b91d940e22645f96d957df31544",
+    "url": "/portfolio/static/media/telephone.2b0c9b91.png"
+  },
+  {
+    "revision": "b48a9f1268de0103c53a203628615346",
+    "url": "/portfolio/static/media/icono.b48a9f12.jpg"
+  },
+  {
+    "revision": "842a918c3ad750d4f3288b6c1a1577c6",
+    "url": "/portfolio/static/media/map-europe.842a918c.png"
+  },
+  {
+    "revision": "a32f83aa2c675058e4a05a0fd4da05eb",
+    "url": "/portfolio/static/media/css.a32f83aa.png"
+  },
+  {
+    "revision": "2c3d6fa6767964a39bffd525a30b8884",
+    "url": "/portfolio/static/media/intellij.2c3d6fa6.png"
+  },
+  {
+    "revision": "003fafdf9ff76fe2e8a95b12eec22829",
+    "url": "/portfolio/static/media/invision.003fafdf.png"
+  },
+  {
+    "revision": "af3a962bf479d16c67ba60e681c2d79e",
+    "url": "/portfolio/static/media/php.af3a962b.png"
+  },
+  {
+    "revision": "fb3735b3f6bc4d4437a422358b89ffb0",
+    "url": "/portfolio/static/media/architecture.fb3735b3.png"
+  },
+  {
+    "revision": "e31816e7eeef9d1234b0503af76ae38e",
+    "url": "/portfolio/static/media/design.e31816e7.png"
+  },
+  {
+    "revision": "7b554fa94a4331dfef4184753509a7fe",
+    "url": "/portfolio/static/media/mobileapp.7b554fa9.png"
+  },
+  {
+    "revision": "2a4a22df2137b63396b76a444c2bc037",
+    "url": "/portfolio/static/media/polymer.2a4a22df.png"
+  },
+  {
+    "revision": "1482da0af731d6167a645cf8200e8654",
+    "url": "/portfolio/static/media/react.1482da0a.png"
+  },
+  {
+    "revision": "0c96bd7898db820ad5e017c4369ae3d6",
+    "url": "/portfolio/static/media/gopopup.0c96bd78.png"
+  },
+  {
+    "revision": "8e9cd278f8fb2ad44e855fb86d70ff56",
+    "url": "/portfolio/static/media/ing.8e9cd278.png"
+  },
+  {
+    "revision": "9c4058a4080b54e7cd9a",
+    "url": "/portfolio/static/css/1.200a9ff9.chunk.css"
+  },
+  {
+    "revision": "2c0c69df84eaf1f23a705503a381161a",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3b9a08b67c84d671c24934f9339bf7a7.js"
+  "/portfolio/precache-manifest.6c0d556b9adbf6ecbd2116792564a97d.js"
 );
 
 workbox.clientsClaim();
