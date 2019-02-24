2a98023ea193bc39583034ea7dc9b19a90d9d109
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2019-02-24 10:53:08 +0100

    feat: implement action buttons in home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom'
+import CloudDownload from '@material-ui/icons/CloudDownload';
 
 const Home = ({ body }) => (
-    <p>{body}</p>
+    <Fragment>
+        <p>{body}</p>
+        <Button variant="contained" 
+                color="default" 
+                component={Link}
+                to="/contact">
+            Contact me
+        </Button>
+        <Button variant="contained" 
+                color="default" 
+                component="a"
+                download="CV_Manuel_delPozo_FrontEndDeveloper"
+                href={require(`./../../assets/CV_Manuel_delPozo_FrontEndDeveloper.pdf`)}>
+            Resume
+            <CloudDownload />
+        </Button>
+    </Fragment>
 )
 
 Home.propTypes = {
