8d99536ee52362cc44d20480f5c87af3b7d36333
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2019-03-03 20:03:21 +0100

    feat:Autofill comment field

--- src/components/forms/asyncValidate.js
+++ src/components/forms/asyncValidate.js
@@ -1,12 +1,31 @@
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+import { change } from 'redux-form'
 
-const asyncValidate = (values /*, dispatch */) => {
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+let introduction = ''
+let farewell = ''
+
+const asyncValidate = (values, dispatch) => {
   return sleep(1000).then(() => {
     // simulate server latency
     if (['mdp.webdeveloper@gmail.com'].includes(values.email)) {
       // eslint-disable-next-line no-throw-literal
       throw { email: 'This is my email address' }
     }
+
+    if (values.fullName) {
+      introduction = `\n\nThis is ${values.fullName}`
+      farewell = `\n\nRegards\n\n${values.fullName}`
+
+      if (values.companyName) {
+        introduction += ` from ${values.companyName}` 
+      }
+
+      if (values.email) {
+        farewell += `\n${values.email}` 
+      }
+    }
+
+    dispatch(change('ContactForm', 'comment', `Hi Manuel,${introduction}${farewell}`));
   })
 }
 
