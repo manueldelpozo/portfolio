a5a668a17307a13d54c1b16beb0d805f28ff93d4
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2019-03-14 21:40:14 +0100

    feat: implement Map component with react-leaflet

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,161 +1,45 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
-
-/*
-const styles = [
-  {
-    stylers: [
-      { hue: "#00ffe5" },
-      { saturation: -20 }
-    ]
-  },{
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [
-      { lightness: 100 },
-      { visibility: "simplified" }
-    ]
-  },{
-    featureType: "road",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "administrative.country",
-    elementType: "labels.text",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "administrative.locality",
-    elementType: "labels.text",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },{
-    featureType: "water",
-    elementType: "labels.text",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },
-  {
-    featureType: "landscape.natural.landcover",
-    elementType: "geometry",
-    stylers: [
-      { visibility: "off" }
-    ]
-  },
-  {
-    featureType: "water",
-    elementType: "geometry.fill",
-    stylers: [
-      { color: "#009999" },
-      { visibility: "on" }
-    ]
-  }
-]
-
-const styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"})
-
-const mapProp = {
-  center:new google.maps.LatLng(52,2),
-  zoom:4,
-  mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style'],
-  mapTypeControl: false,
-  panControl: false,
-  zoomControl: false,
-  scaleControl: false,
-  streetViewControl: false
-} */
+import './../../node_modules/leaflet/dist/leaflet.css'
 
 class MapContainer extends PureComponent {
   constructor(props) {
     super(props)
-    this.markers = [
-      {
-        "city": "Clermont-Ferrand",
-        "lat": 45.777222,
-        "lng": 3.087025,
-        "info": "<strong>WEB DESIGNER </strong><br> AU 10 ET DÉCO - CLERMONT FERRAND - 2011",
-        "icon": "icons/maps/works-au10etdeco.png"
-      },
-      {
-        "city": "Murcia",
-        "lat": 37.992240,
-        "lng": -1.130654,
-        "info": "<strong>UI/UX DESIGNER AND FRONT-END DEVELOPER </strong><br> QUESERADEMI (STARTUP) - MURCIA - 2012/18",
-        "icon": "icons/maps/works-qsdm.png"
-      },
-      {
-        "city": "Berlin",
-        "lat": 52.520007,
-        "lng": 13.404954,
-        "info": "<strong>WEB DEVELOPER </strong><br> GO-POPUP - BERLIN - 2015",
-        "icon": "icons/maps/works-gopopup.png"
-      },
-      {
-        "city": "Leszno",
-        "lat": 51.829760,
-        "lng": 16.597281,
-        "info": "<strong>JAVASCRIPT UI DEVELOPER </strong><br> C&C TECHNOLOGY - LESZNO - 2015/16",
-        "icon": "icons/maps/works-cctechnology.png"
-      },
-      {
-        "city": "Madrid",
-        "lat": 40.416775,
-        "lng": -3.703790,
-        "info": "FRONT-END DEVELOPER__BANKIA (GFT GROUP)__MADRID__2017",
-        "icon": "icons/maps/works-bankia.png"
-      },
-      {
-        "city": "Brussels",
-        "lat": 50.8503396,
-        "lng": 4.3517103,
-        "info": "FRONT-END DEVELOPER__ING BANK (COGNIZANT)__BRUSSELS__2017/18",
-        "icon": "icons/maps/works-ing.png"
-      }
-    ]
+    props.places.map(place => this[place.location.city] = React.createRef())
     this.state = {
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 4
+      lat: 47,
+      lng: 5,
+      zoom: 4,
+      places: props.places
     }
-    
-    this.onMarkerClick = this.onMarkerClick.bind(this)
-    this.onMapClick = this.onMapClick.bind(this)
   }
 
-  onMarkerClick = (props, marker, e) => {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-  }
-
-  onMapClick = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
-    }
+  componentDidMount() {
+    setTimeout(() => {
+      this.state.places.map(place => {
+        const iconEl = this[place.location.city].current.leafletElement._icon
+        iconEl.setAttribute('src', iconEl.getAttribute('src').split('")')[0])
+      })
+    }, 0)
   }
 
   render() {
-    const markers = this.markers.map(marker => (
-      <Marker position={[marker.lat, marker.lng]} key={`marker_${marker.name}`}>
-        <Popup>
-          <span>{marker.city}</span>
+    const markers = this.state.places.map(place => (
+      <Marker position={[place.location.lat, place.location.lng]} 
+              key={`marker_${place.location.city}`}
+              ref={this[place.location.city]}>
+        <Popup className="App-map__marker-popup">
+          <img src={require(`./../assets/${place.icon}`)} height="auto" width="50" alt={place.title} />
+          <Typography align="center" color="primary" paragraph>{place.location.city}</Typography>
         </Popup>
       </Marker>
-    ));
+    ))
 
     return (
-      <div className="map">
+      <div className="App-map">
         <Map center={[this.state.lat, this.state.lng]} zoom={this.state.zoom} className="map__reactleaflet">
           <TileLayer
             url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
@@ -164,8 +48,12 @@ class MapContainer extends PureComponent {
           {markers}
         </Map>
       </div>
-    );
+    )
   }
 }
 
-export default MapContainer;
+MapContainer.propTypes = {
+  places: PropTypes.arrayOf(PropTypes.object)
+}
+
+export default MapContainer
--- src/components/pages/Works.js
+++ src/components/pages/Works.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Grid from './../Grid.js'
-//import MapContainer from './../MapContainer.js'
+import MapContainer from './../MapContainer.js'
 
 const Works = ({ body }) => (
     <Fragment>
         <Grid list={body} />
-        
+        <MapContainer places={body} />
     </Fragment>
 )
 
